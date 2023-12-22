8a61415a49ec5fb6bb82d1457702d61fb798f715
Colin Bagley <cjbagley@googlemail.com>
2023-12-22 20:29:26 +0000

    [WIP] Adding additional video tests

--- cmd/video_test.go
+++ cmd/video_test.go
@@ -4,9 +4,11 @@ import (
 	"testing"
 )
 
-func TestNewVideoCommand(t *testing.T) {
+// Test a few arguments are being set, some set by default, others by user above
+// Not sure how useful this test is, but this is more for practice
+func TestNewVideoCommandArgs(t *testing.T) {
 	cmd := NewVideoCommand()
-	cmd.Init([]string{"-fs", "20"})
+	cmd.Init([]string{"-fs", "20", "-o", "processed-file"})
 
 	if cmd.fromSeconds != 20 {
 		t.Errorf("VideoCommand.fromSeconds = %v, expected %v", cmd.fromSeconds, 0)
@@ -18,5 +20,48 @@ func TestNewVideoCommand(t *testing.T) {
 
 	if cmd.toSeconds != 0 {
 		t.Errorf("VideoCommand.toSeconds = %v, expected %v", cmd.fromSeconds, 0)
+	}
+
+	if cmd.outputName != "processed-file" {
+		t.Errorf("VideoCommand.output = %v, expected %v", cmd.outputName, "processed-file")
+	}
+}
+
+func TestVideoValidateFlags(t *testing.T) {
+	var err error
+
+	cmd1 := NewVideoCommand()
+	cmd1.Init([]string{"-fs", "61"})
+	err = cmd1.ValidateFlags()
+	if err == nil {
+		t.Errorf("VideoCommand.validateFlags allowed 61 seconds and did not error")
+	}
+
+	cmd2 := NewVideoCommand()
+	cmd2.Init([]string{"-o", ""})
+	err = cmd2.ValidateFlags()
+	if err == nil {
+		t.Errorf("VideoCommand.validateFlags allowed empty output and did not error")
+	}
+
+	cmd3 := NewVideoCommand()
+	cmd3.Init([]string{"-crf", "2000"})
+	err = cmd3.ValidateFlags()
+	if err == nil {
+		t.Errorf("VideoCommand.validateFlags allowed incorrect crf and did not error")
+	}
+
+	cmd4 := NewVideoCommand()
+	cmd4.Init([]string{})
+	err = cmd4.ValidateFlags()
+	if err != nil {
+		t.Errorf("VideoCommand.validateFlags returned error when no arguments present")
+	}
+
+	cmd5 := NewVideoCommand()
+	cmd5.Init([]string{"-o", "new-output name", "ts", "40", "tm", "1"})
+	err = cmd5.ValidateFlags()
+	if err != nil {
+		t.Errorf("VideoCommand.validateFlags returned error when valid arguments present")
 	}
 }
